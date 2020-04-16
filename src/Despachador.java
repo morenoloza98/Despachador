7eb745e214cf1b4ce81ab314513f6ecc76969c92
morenoloza98 <morenolozalex98@gmail.com>
2020-04-15 19:42:58 -0500

    Last changes

--- src/Despachador.java
+++ src/Despachador.java
@@ -51,9 +51,10 @@ public class Despachador
 
     public void deleteHole(){
         for (Micro micro : listaMicros) {
-            if(micro.getTerminados().get(micro.getTerminados().size()-1).getId() == "Hueco")
+            if(micro.getTerminados().get(micro.getTerminados().size()-1).getId() == "Hueco"){
                 micro.setTiempoTotal(micro.getTiempoTotal() - (micro.getTerminados().get(micro.getTerminados().size()-1)).getExeTime());
                 micro.getTerminados().remove(micro.getTerminados().size()-1);
+            }
         }
     }
 
